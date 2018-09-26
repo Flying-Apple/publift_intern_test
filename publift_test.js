144fea190389852e843a791b72a3f6fd968047d1
Shiwei Sun <33053669+Flying-Apple@users.noreply.github.com>
2018-09-26 11:19:52 +1000

    clean up a bit

--- publift_test.js
+++ publift_test.js
@@ -11,69 +11,73 @@ http.createServer(function (req, res) {
             res.writeHead(404, {'Content-Type': 'text/html'});
             return res.end("404 Not Found");
         }  
-        
+        i=0;
+        var ispng=0;
+        var issvg=0;
+
         if(q.pathname == "/question2" && q.search.substring(0,9)=="?content="){         //get content here
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(q.search.substring(9));
             return res.end();
         }
+
+        else if (q.pathname == "/question3" && req.method == 'POST') {     //get post message
+            body='';
+            req.on('data', function (chunk) {
+                body+=chunk.toString('base64');
+                if(i<1){
+                    if (chunk.slice(0,8).equals(png)){          //check first 8 bytes to see if the file is PNG [137,80,78,71,13,10,26,10]
+                        ispng=1;
+                        console.log("good file");
+                    }
+                    else if (chunk.toString().substring(0,100).includes("svg")){    //check if first few lines contain "svg"
+                                                                                              //not a good practice, can be tricked by txt file with "svg" in it
+                        issvg=1;
+                    }
+                    else{
+                        console.log("not valid file");
+                        return res.end();
+                    }
+                }
+                i=1;
+            });
+            req.on('end', function() {
+                var decodeImg = new Buffer(body, 'base64')
+                if (ispng==1 || issvg==1){
+                    if (ispng==1){
+                        var filename1 = "Received_png.png";
+                        writeFile();
+                    }   
+                    else if(issvg==1){
+                        var filename1 = "Received_svg.svg";
+                        writeFile();            
+                    }
+                    function writeFile() {                          //png may not fully written here
+                        if ((fs.existsSync(filename1))) {
+                            console.log("file " + filename1 + "exists, use a new name");
+                            filename1 ="0"+ filename1;
+                            writeFile();
+                        }
+                        else {
+                            fs.writeFile(filename1, decodeImg,'base64', function(err){
+                                if (err){
+                                    console.log(err)
+                                }
+                                else{
+                                    console.log("done: " + filename1);
+                                    return res.end();
+                                }
+                                })
+                        }             
+                    }
+                }
+            });
+          }
         else{
             res.writeHead(200, {'Content-Type': 'text/html'});      //open page here
             res.write(data);
             return res.end();
         }
     });
-    i=0;
-    var ispng=0;
-    var issvg=0;
-    if (q.pathname == "/question3" && req.method == 'POST') {     //get post message
-    body='';
-    req.on('data', function (chunk) {
-        body+=chunk.toString('base64');
-        if(i<1){
-            if (chunk.slice(0,8).equals(png)){          //check first 8 bytes to see if the file is PNG [137,80,78,71,13,10,26,10]
-                ispng=1;
-                console.log("good file");
-            }
-            else if (chunk.toString().substring(0,100).includes("svg")){    //check if first few lines contain "svg"
-                                                                                      //not a good practice, can be tricked by txt file with "svg" in it
-                issvg=1;
-            }
-            else{
-                console.log("not valid file");
-            }
-        }
-        i=1;
-    });
-    req.on('end', function() {
-        var decodeImg = new Buffer(body, 'base64')
-        if (ispng==1 || issvg==1){
-            if (ispng==1){
-                var filename1 = "Received_png.png";
-                writeFile();
-            }   
-            else if(issvg==1){
-                var filename1 = "Received_svg.svg";
-                writeFile();            
-            }
-            function writeFile() {                          //png may not fully written here
-                if ((fs.existsSync(filename1))) {
-                    console.log("file " + filename1 + "exists, use a new name");
-                    filename1 ="0"+ filename1;
-                    writeFile();
-                }
-                else {
-                    fs.writeFile(filename1, decodeImg,'base64', function(err){
-                        if (err){
-                            console.log(err)
-                        }
-                        else{
-                            console.log("done: " + filename1);
-                        }
-                        })
-                }             
-            }
-        }
-    });
-  }
+
 }).listen(8080);
